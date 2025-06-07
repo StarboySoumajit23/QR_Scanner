d6182dee6add8e006d31fa4e4cb240cb12918dd3
Soumajit <soumajit2764@gmail.com>
2025-06-07 10:55:15 +0530

    Initial Commit

---
+++ practise/a.js
@@ -0,0 +1,20 @@
+console.log("This is learning javascript course")
+let a=45;
+
+let b=null; 
+let c="ok";
+let d=undefined;
+let e=BigInt("45");
+let f=true;
+let g=Symbol("I am a nice boy");
+const name={
+"Souma":43,
+};
+
+
+console.log(a,b,c,d,e,f,g)
+console.log(name["Souma"])
+let h=2;
+console.log(--a)
+let s=`boy1 is a friend of boy1`
+console.log(s)
